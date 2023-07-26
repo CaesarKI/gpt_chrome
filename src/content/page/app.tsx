5b829dd5b241fd901e65668f856918f4e909649d
hui.liu15 <2365906421@qq.com>
2023-07-26 10:55:42 +0800

    test: 测试数据2

--- src/content/page/app.tsx
+++ src/content/page/app.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect } from 'react'
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
 import store from '../store/index'
-import 'antd/dist/reset.css';
-import Panel from '../components/index';
+import 'antd/dist/reset.css'
+import Panel from '../components/index'
 import './app.less'
-import { tag, containerId } from '../shdow-dom';
+import { tag, containerId } from '../shdow-dom'
 import browser from 'webextension-polyfill'
-import { Configuration, OpenAIApi } from 'openai';
-
+import { Configuration, OpenAIApi } from 'openai'
 
 export default function App() {
   const container = document.querySelector(tag)
@@ -22,48 +21,51 @@ export default function App() {
         flag = false
         return
       }
-      var card = shadowRoot.querySelector('#card');
+      var card = shadowRoot.querySelector('#card')
       if (card && container !== event.target) {
-        card.remove();
+        card.remove()
       }
     }
 
     const handelCardClick = (event: any) => {
       const target = event.target
-      const parentElement = target.parentNode;
+      const parentElement = target.parentNode
       createContainer(parentElement)
       target.remove()
       event.stopPropagation()
     }
 
     document.addEventListener('mouseup', function (event: any) {
+      const name = 'zs'
       const child = root.querySelector('#card')
       if (child) return
       clearTimeout(timer)
-      let selectedText = window.getSelection()?.toString();
+      let selectedText = window.getSelection()?.toString()
       //@ts-ignore
-      const range: any = window.getSelection()?.getRangeAt(0);
-      const rect = range.getBoundingClientRect();
+      const range: any = window.getSelection()?.getRangeAt(0)
+      const rect = range.getBoundingClientRect()
       if (selectedText) {
         store.setValue('text', selectedText)
-        var card = document.createElement('div');
+        var card = document.createElement('div')
         const btn = document.createElement('div')
         btn.setAttribute('id', 'btn')
         card.setAttribute('id', 'card')
-        btn.textContent = 'w';
+        btn.textContent = 'w'
         card.appendChild(btn)
-        card.style.cssText = `left:${Math.abs(rect.left - 32)}px;top:${rect.top + rect.height}px`
+        card.style.cssText = `left:${Math.abs(rect.left - 32)}px;top:${
+          rect.top + rect.height
+        }px`
         flag = true
-        root.appendChild(card);
+        root.appendChild(card)
         btn.addEventListener('click', handelCardClick)
         // timer=setTimeout(()=>{
         //   const event=new Event('click')
         //   document.dispatchEvent(event)
         // },3000)
       }
-    });
+    })
 
-    document.addEventListener('click', handelClick);
+    document.addEventListener('click', handelClick)
   }
 
   const createContainer = (parentElement: Element) => {
@@ -74,15 +76,14 @@ export default function App() {
     containerRoot.render(<Panel />)
   }
 
-
   const createCard = () => {
-    store.setValue('text', "")
+    store.setValue('text', '')
     const child = root.querySelector('#card')
     if (child) return
-    var card = document.createElement('div');
+    var card = document.createElement('div')
     card.setAttribute('id', 'card')
     card.style.cssText = `left:50%;top:30%;transform:translate(-50%,-50%)`
-    root.appendChild(card);
+    root.appendChild(card)
     createContainer(card)
   }
 
@@ -94,27 +95,29 @@ export default function App() {
   }
 
   const getkey = async () => {
-    const value = await browser.storage.sync.get('openai');
+    const value = await browser.storage.sync.get('openai')
     store.setValue('openai', value.openai)
     const gpt = generateGpt(value.openai.key)
     store.setValue('gpt', gpt)
   }
 
   const handelMessage = () => {
-    browser.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-      if (message.action === "open-content") {
+    browser.runtime.onMessage.addListener(function (
+      message,
+      sender,
+      sendResponse
+    ) {
+      if (message.action === 'open-content') {
         // 执行打开内容的操作
         createCard()
-        console.log("Open content triggered by shortcut key");
+        console.log('Open content triggered by shortcut key')
       }
-    });
+    })
   }
-
 
   useEffect(() => {
     handelEvent()
     handelMessage()
     getkey()
   }, [])
-
 }
