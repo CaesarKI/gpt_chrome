591a80437bb59af6ca3e0a9eddb1f3f9020052ea
hui.liu15 <2365906421@qq.com>
2023-08-01 14:31:24 +0800

    feat: 优化指令

--- src/content/instruct/index.ts
+++ src/content/instruct/index.ts
@@ -2,11 +2,11 @@ export default {
   map: [
     {
       reg: /(\$1)$/,
-      label: '将这些英文翻译为中文',
+      label: '翻译为中文',
     },
     {
       reg: /(\$2)$/,
-      label: '将这些中文翻译为英文',
+      label: '翻译为英文',
     },
   ],
 }
